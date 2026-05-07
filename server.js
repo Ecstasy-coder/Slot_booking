c26c781d9b3f0e3c4b635e3d82949ecf9057cb70
Ecstasy-coder <ecstasysolutions123@gmail.com>
2026-05-07 12:35:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
+
 require("dotenv").config();
 
 const app = express();
@@ -9,33 +11,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve frontend files
+app.use(express.static(path.join(__dirname)));
+
 // Routes
-const slotRoutes =
-require("./routes/slotRoutes");
+const slotRoutes = require("./routes/slotRoutes");
 
 app.use("/api", slotRoutes);
 
 // MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-mongoose.connect(process.env.MONGO_URI)
-
-.then(() =>
-console.log("MongoDB Connected"))
-
-.catch(err =>
-console.log(err));
-
-// Test route
-
+// Frontend route
 app.get("/", (req, res) => {
-  res.send("Backend Running");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = 5000;
+// Start server
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
-  console.log(
-  `Server running on ${PORT}`);
-
+  console.log(`Server running on ${PORT}`);
 });
